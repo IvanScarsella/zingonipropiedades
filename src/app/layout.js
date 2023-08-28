b8be04ebbacb2e68c8fd0f765151d277a25a0fc4
Iván Scarsella <scarsellaivan@gmail.com>
2023-08-28 14:49:23 -0300

    fix head

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,21 @@
 import './globals.css';
 import { GlobalContextProvider } from '../../context/store';
+import favicon from "../../public/favicon.ico";
 
 export default function RootLayout({
   children,
-}) 
-
-{
+}) {
   return (
     <html lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <title>Zingoni Propiedades</title>
+        <link rel="shortcut icon" href={favicon.src} />
+        {/* Otros elementos head si los tienes */}
+      </head>
       <body>
         <GlobalContextProvider>{children}</GlobalContextProvider>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,42 +1,8 @@
 "use client";
 
-// import { appendMutableCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
-// import { APP_BUILD_MANIFEST } from "next/dist/shared/lib/constants";
-import { useEffect } from 'react';
-import axios from 'axios';
-import Home from "./home/page";
 import Landing from "./landing/page";
 
 function App() {
-//   // useEffect para configurar CORS cuando la aplicación se inicia
-//   useEffect(() => {
-//     // Configuración de CORS en Cloudinary
-//     const cloudName = process.env.CLOUD_NAME;
-//     const apiKey = process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY;
-//     const apiSecret = process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET;
-
-//     const corsConfig = {
-//       allowed_origins: ['*'],
-//       allowed_headers: ['*'],
-//       allowed_methods: ['POST', 'GET', 'PUT', 'DELETE'],
-//     };
-
-//     const configUrl = `https://api.cloudinary.com/v1_1/${cloudName}/upload/cors`;
-
-//     axios.post(configUrl, corsConfig, {
-//       auth: {
-//         username: apiKey,
-//         password: apiSecret,
-//       },
-//     })
-//       .then((response) => {
-//         console.log('Configuración de CORS en Cloudinary realizada con éxito:', response.data);
-//       })
-//       .catch((error) => {
-//         console.error('Error al configurar CORS en Cloudinary:', error.message);
-//       });
-//   }, []);
-
   return <Landing />;
 }
 
