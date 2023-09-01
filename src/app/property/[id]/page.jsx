e8630e3c95993d217a815442f89317500fecc709
Iván Scarsella <scarsellaivan@gmail.com>
2023-09-01 13:16:46 -0300

    ...

--- src/app/property/[id]/page.jsx
+++ src/app/property/[id]/page.jsx
@@ -22,6 +22,7 @@ import antiquity_logo from "../../../../public/antiquity_logo.png";
 import location_logo from "../../../../public/location_logo.png";
 import MailForm from "../../components/mailForm/page";
 
+
 function PropertyByID(id) {
     const router = useRouter();
     console.log(id);
@@ -188,6 +189,7 @@ function PropertyByID(id) {
                         <h3><Image src={antiquity_logo} alt='property image' width='20' height='20'/>   Antigüedad: {property.antiquity} años</h3>
                         : null}
                 </div>
+
                 <MailForm id={id}/>
                 {/* <form onSubmit={handleSubmit}>
                     <div>
