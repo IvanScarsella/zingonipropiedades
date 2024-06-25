ab9a4925373ec72ed9801843b2acb0da61f3d614
Iván Scarsella <scarsellaivan@gmail.com>
2024-06-25 16:51:36 -0300

    translate landing to tailwind

--- src/app/landing/page.jsx
+++ src/app/landing/page.jsx
@@ -30,46 +30,90 @@ export default function Landing() {
     }, [properties])
 
     return (
-        <div className={styles.landingContainer}>
-            <div className={styles.headerContainer}>
-                <div className={styles.headerLogo}>
+        <div
+            className='flex flex-col items-center'
+        //  className={styles.landingContainer}
+        >
+            <div
+                className='flex flex-row justify-items-center justify-center min-w-full mb-10 bg-gradient-custom shadow-lg shadow-black max-[600px]:flex-col max-[600px]:items-center max-[600px]:p-10'
+            // className={styles.headerContainer}
+            >
+                <div
+                    className='flex rounded-2xl grid-cols-2 mb-0 order-2 items-center'
+                // className={styles.headerLogo}
+                >
                     <Image
                         src={landing_logo}
                         alt="landing_logo"
                         width={400}
                         height={400}
-                        className="rounded-image"
+                        className="mx-[100px] drop-shadow-lg"
                         onClick={() => router.push('/home')}
                     />
                 </div>
-                <div className={styles.headerButtons1}>
+                <div
+                    className='flex order-1 flex-col justify-around items-center'
+                //  className={styles.headerButtons1}
+                >
 
                     <div>
-                        <button className={`${styles.headerButton}`} onClick={() => router.push('/home?type=Venta')}><span>Ventas</span></button>
+                        <button
+                            className='border-0 bg-gradient-custom-2 rounded-lg text-white flex text-4xl cursor-pointer transition-all items-center justify-center drop-shadow-lg active:scale-90'
+                            //  className={`${styles.headerButton}`} 
+                            onClick={() => router.push('/home?type=Venta')}>
+                            <span
+                                className='bg-gradient-custom bg-[#693d7ab0] px-4 py-6 rounded-md transition-all w-[200px] hover:bg-none hover:scale-[1.2]'
+                            >Ventas</span></button>
                     </div>
                     <div>
-                        <button className={`${styles.headerButton}`} onClick={() => router.push('/home?type=Alquiler')}><span>Alquileres</span></button>
+                        <button
+                            className='border-0 bg-gradient-custom-2 rounded-lg text-white flex text-4xl cursor-pointer transition-all items-center justify-center drop-shadow-lg active:scale-90'
+                            //  className={`${styles.headerButton}`}
+                            onClick={() => router.push('/home?type=Alquiler')}><span
+                                className='bg-gradient-custom bg-[#693d7ab0] px-4 py-6 rounded-md transition-all w-[200px] hover:bg-none hover:scale-[1.2]'
+                            >Alquileres</span></button>
                     </div>
                 </div>
-                <div className={styles.headerButtons2}>
+                <div
+                    className='flex order-3 flex-col justify-around items-center'
+                //  className={styles.headerButtons2}
+                >
 
                     <div>
-                        <button className={`${styles.headerButton}`} onClick={() => router.push('/auxiliar')}><span>Auxiliares</span></button>
+                        <button
+                            className='border-0 bg-gradient-custom-2 rounded-lg text-white flex text-4xl cursor-pointer transition-all items-center justify-center drop-shadow-lg active:scale-90'
+                            //  className={`${styles.headerButton}`}
+                            onClick={() => router.push('/auxiliar')}><span
+                                className='bg-gradient-custom bg-[#693d7ab0] px-4 py-6 rounded-md transition-all w-[200px] hover:bg-none hover:scale-[1.2]'
+                            >Auxiliares</span></button>
                     </div>
                     <div>
-                        <button className={`${styles.headerButton}`} onClick={() => router.push('/contact')}><span>Contacto</span></button>
+                        <button
+                            className='border-0 bg-gradient-custom-2 rounded-lg text-white flex text-4xl cursor-pointer transition-all items-center justify-center drop-shadow-lg active:scale-90'
+                            // className={`${styles.headerButton}`} 
+                            onClick={() => router.push('/contact')}><span className='bg-gradient-custom bg-[#693d7ab0] px-4 py-6 rounded-md transition-all w-[200px] hover:bg-none hover:scale-[1.2]'
+                            >Contacto</span></button>
                     </div>
                 </div>
             </div>
-            <h1 className={styles.featuredPropertiesTitle}>Propiedades Destacadas</h1>
-            <div className={styles.featuredProperties}>
+            <h1
+                className='text-white flex justify-center items-center text-4xl bg-gradient-custom bg-[#693d7ab0] drop-shadow-md min-w-full'
+            //  className={styles.featuredPropertiesTitle}
+            >Propiedades Destacadas</h1>
+            <div
+                className='ml-[1px]'
+            //  className={styles.featuredProperties}
+            >
                 {propertiesChunks.length ?
                     <FeaturedPropertiesCarousel
                         properties={propertiesChunks}
                     />
                     : <p>Cargando propiedades...</p>}
             </div>
-            <div className={styles.mailForm}>
+            <div
+                className='flex items-center justify-center m-1'
+            //  className={styles.mailForm}
+            >
                 <MailForm />
             </div>
             <Footer />
